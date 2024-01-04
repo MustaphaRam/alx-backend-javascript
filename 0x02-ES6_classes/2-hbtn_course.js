da5596d0b384bd59a61a64ff7cd10db7c9eb7030
MustaphaRam <mustapha.mad.75@gmail.com>
2024-01-04 15:45:11 +0100

    0x02-ES6_classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('Studentsmust be a array');
+    }
+    this._students = students;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._amount;
+  }
+
+  set floors(floors) {
+    this._currency = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
